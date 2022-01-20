1715400b4e3234bbc2545d9f867f55782c6ac7b8
brian-lets-go <brian.w.meenan@gmail.com>
2022-01-19 21:58:41 -0500

    draft javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+var cardIndex = 0;
+var currentCard = cards[cardIndex];
+
 var cards = [
     {
         question: "question 1",
@@ -6,10 +9,45 @@ var cards = [
     {
         question: "question 2",
         answer: "answer 2"       
+    },
+    {
+        question: "question 3",
+        answer: "answer 3"   
     }
 ]
 
-var questionEl = document.querySelector("#index-card-body");
+
+var questionEl = document.querySelector("#question");
 var answerEl = document.querySelector("#answer");
 var answerButton = document.querySelector("#answer-button");
+var nextButton = document.querySelector("#next-button");
 
+function displayQuestion() {
+
+    console.log("question");
+    
+    for (var i = 0; i < cards.length; i++) {
+    
+        var pTag = document.createElement("p");
+
+        pTag.textContent = cards[i].question;
+
+        questionEl.appendChild(pTag);
+
+        cardIndex++;
+
+    }
+    
+}
+
+function revealAnswer() {
+
+    console.log("answer");
+
+    currentCard.answer.setAttribute("class", "show");
+
+}
+
+answerButton.onclick = revealAnswer()
+
+nextButton.onclick = displayQuestion()
