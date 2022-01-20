163c302ac865fff6fb00d3a12c05c745a5eb2eb9
brian-lets-go <brian.w.meenan@gmail.com>
2022-01-19 19:36:44 -0500

    first update

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+var cards = [
+    {
+        question: "question 1",
+        answer: "answer 1"
+    },
+    {
+        question: "question 2",
+        answer: "answer 2"       
+    }
+]
+
+var questionEl = document.querySelector("#index-card-body");
+var answerEl = document.querySelector("#answer");
+var answerButton = document.querySelector("#answer-button");
+
